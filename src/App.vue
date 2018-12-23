e79877ba5c144d54956ccf8b015212ee4c4e0e0e
Takao <>
2018-12-23 15:25:09 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h2>タスク一覧</h2>
+    <ul>
+      <li v-for="task in tasks" v-bind:key="task.id">
+        <input type="checkbox" v-bind:checked="task.done">
+          {{ task.name }}
+      </li>
+    </ul>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  computed: {
+    tasks () {
+      return this.$store.state.tasks
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
