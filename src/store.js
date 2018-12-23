32db23bf8aff9717db49b2fd8bf4e81a3a494db9
Takao <sukisya70@gmail.com>
2018-12-23 17:25:24 +0900

    label filter機能実装

--- src/store.js
+++ src/store.js
@@ -9,21 +9,50 @@ const store = new Vuex.Store({
       {
         id: 1,
         name: '牛乳を買う',
+        labelIds: [1, 2],
         done: false
       },
       {
         id: 2,
         name: 'Vue.jsの本を買う',
+        labelIds: [1, 3],
         done: true
       }
     ],
-    nextTaskId: 3
+    labels: [
+      {
+        id: 1,
+        text: '買い物'
+      },
+      {
+        id: 2,
+        text: '食料'
+      },
+      {
+        id: 3,
+        text: '本'
+      }
+    ],
+    nextTaskId: 3,
+    nextLabelId: 4,
+    filter: null
+  },
+  getters: {
+    filteredTasks (state) {
+      if (!state.filter) {
+        return state.tasks
+      }
+      return state.tasks.filter(task => {
+        return task.labelIds.indexOf(state.filter) >= 0
+      })
+    }
   },
   mutations: {
-    addTask (state, { name }) {
+    addTask (state, { name, labelIds }) {
       state.tasks.push({
         id: state.nextTaskId,
         name,
+        labelIds,
         done: false
       })
       state.nextTaskId++
@@ -35,6 +64,16 @@ const store = new Vuex.Store({
       filtered.forEach(task => {
         task.done = !task.done
       })
+    },
+    addLabel (state, { text }) {
+      state.labels.push({
+        id: state.nextLabelId,
+        text
+      })
+      state.nextLabelId++
+    },
+    changeFilter (state, { filter }) {
+      state.filter = filter
     }
   }
 })
