82b76e82bde744908dbb6c602f94ed5d08ab52be
Takao <sukisya70@gmail.com>
2018-12-31 13:56:57 +0900

    vuex, component

---
+++ src/this_store.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>{{ count }}</p>
+    <button v-on:click="increment(1)">+1</button>    
+  </div>
+</template>
+
+<script>
+  import { mapState, mapMutations } from 'vuex'
+
+  export default {
+    // computed: {
+    //   count () {
+    //     return this.$store.state.count
+    //   }
+    // },
+    computed: mapState([
+      // 'count'
+      value: 'count'
+    ])
+    // methods: {
+    //   increment (value) {
+    //     this.$store.commit('increment', value)
+    //   }
+    // }
+    methods: mapMutations([
+      'increment'
+    ])
+  }
+</script>
